7b0f2335cf91ad04a9ca6fb716eb75a3b8d23b95
sumit-616 <sumit616794@gmail.com>
2025-02-27 04:09:07 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        singleDay: ['"Single Day"', 'cursive'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
