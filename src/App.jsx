5bee0a8b74553c89e2e296e784294465eabf7cb9
sumit-616 <sumit616794@gmail.com>
2025-02-28 19:43:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import React from "react";
-import Home from "./Components/Home";
+import Login from "./Components/Login";
+import Genre from "./Components/Genre";
+import Widget from "./Components/Widget";
+import { Toaster } from "react-hot-toast";
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function App(){
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />
+    },
+    {
+      path: "/genre",
+      element: <Genre />
+    },
+    {
+      path: "/widget",
+      element: <Widget />
+    }
+  ])
+
+  return <>
+    <ToastContainer />
+    <RouterProvider router={router} />
+  </>;
 }
 
 export default App;
