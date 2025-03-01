ff2834d979d4fa0cc726bb9976d1eb1edbc72875
sumit-616 <sumit616794@gmail.com>
2025-03-01 17:33:38 +0530

    Update Movie.jsx

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Movie = (props) => {
   return (
-    <div style={{ backgroundColor: props.bgColor }} className="w-48 h-[7.7rem] flex flex-col items-center justify-center p-2 rounded-lg text-white transition-transform duration-100 hover:scale-105">
+    <div onClick={()=>props.handleClickForGenre(props.movieGenre)} style={{ backgroundColor: props.bgColor }} className="w-48 h-[7.7rem] flex flex-col items-center justify-center p-2 rounded-lg text-white transition-transform duration-100 hover:scale-105">
       <p className="text-xl font-bold mb-2">{props.movieGenre}</p>
       <img src={props.imgPath} alt="Movie Image" className=" w-[8.5rem] h-[4.8rem] object-fill rounded-sm" />
     </div>
