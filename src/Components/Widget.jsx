0f643c84ffe6c1ff21c2707a820abb461131c0ec
sumit-616 <sumit616794@gmail.com>
2025-03-13 16:19:19 +0530

    Update Widget.jsx

--- src/Components/Widget.jsx
+++ src/Components/Widget.jsx
@@ -1,28 +1,101 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import Stopwatch from './Stopwatch';
+import { useNavigate } from 'react-router-dom';
+import News from './News';
 
 const Widget = () => {
+  const navigate = useNavigate();
   const location = useLocation();
-  const selectedGenres = location.state?.selectedGenres;
+  const { selectedGenres, userData } = location.state || {};
   const [contentWidth, setContentWidth] = useState('100vw');
+
+  const[currentDate, setCurrentDate] = useState(getCurrentDate());
+  const[currentTime, setCurrentTime] = useState(getCurrentTime());
+
+  function getCurrentDate(){
+    const todayDate = new Date();
+    const formattedDate = `${String(todayDate.getDate()).padStart(2, '0')}-${String(todayDate.getMonth() + 1).padStart(2, '0')}-${todayDate.getFullYear()}`;
+    return formattedDate;
+  }
+
+  function getCurrentTime(){
+    const latestTime = new Date();
+    const hours = latestTime.getHours();
+    const minutes = String(latestTime.getMinutes()).padStart(2, '0');
+    const seconds = String(latestTime.getSeconds()).padStart(2, '0');
+
+    const period = hours >= 12 ? 'PM' : 'AM';
+    const formattedHours = String(hours % 12 || 12).padStart(2, '0');
+
+    const formattedTime = `${formattedHours}:${minutes}:${seconds} ${period}`;
+    return formattedTime;
+  }
+
+  setInterval(() => {
+    setCurrentDate(getCurrentDate());
+  }, 10000);
+
+  setInterval(()=>{
+    setCurrentTime(getCurrentTime());
+  },1000);
 
   useEffect(() => {
     const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
     setContentWidth(`calc(100vw - ${scrollbarWidth}px)`);
   }, []);
 
+  function handleClick(){
+    navigate('/movies', {state: {selectedGenres, userData}})
+  }
+
   return (
-    <div className="h-screen bg-black overflow-y-auto" style={{ width: contentWidth }}>
-      <div className='mx-3'>
-        <div className="h-[140vh] grid md:grid-cols-3 md:grid-rows-5 gap-x-6 gap-y-4">
-          <div className="bg-amber-500 row-span-2"></div>
-          <div className="bg-green-500 row-span-3"></div>
-          <div className="bg-red-500 row-span-5"></div>
-          <div className="bg-purple-500"></div>
-          <div className="bg-blue-500 row-span-2 col-span-2"></div>
+    <div className="h-screen w-[100vw] bg-black overflow-y-auto" style={{ width: contentWidth }}>
+      <div className='mx-6 mt-8 mb-2'>
+        <div className="h-[140vh] grid md:grid-cols-3 md:grid-rows-5 sm:grid-cols-1 gap-x-6 gap-y-4">
+          {/* userDp first one */}
+          <div className="flex items-center bg-[#5746EA] row-span-2 py-6 rounded-2xl px-6 ">
+            <img className='h-full w-28' src="/card_img/boydp.png" alt="" />
+            <div className='flex flex-col px-8 gap-4'>
+              <div className='text-white'>
+                <h3 className='text-xl'>{userData?.name}</h3>
+                <h3 className='text-xl'>{userData?.email}</h3>
+                <h1 className='text-3xl font-bold'>{userData?.userName}</h1>
+              </div>
+              <div className='grid md:grid-cols-2 gap-2'>
+                {selectedGenres?.map((genre, index) => (
+                  <div key={index}>
+                    <div className='text-white rounded-2xl text-md h-8 bg-[#9F94FF] flex justify-center items-center'>{genre}</div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          {/* notes second one */}
+          <div className="flex flex-col gap-4 bg-[#F1C75B] row-span-3 p-8 rounded-2xl">
+            <h1 style={{ font: "Roboto" }} className='text-black text-3xl font-bold'>All notes</h1>
+            <textarea className='w-full h-full p-2 border-2 rounded-lg' name="notes" id="notes" placeholder='This is how I am going to learn MERN Stack in next 3 months.'></textarea>
+          </div>
+          {/* news section */}
+          <div className="bg-white row-span-5 rounded-2xl overflow-hidden">
+            <News />
+          </div>
+          {/* weather and time */}
+          <div className='rounded-2xl overflow-hidden text-white'>
+            <div className='flex items-center justify-around text-2xl font-semibold px-6 h-[27%] bg-[#FF4ADE]'>
+              <h1>{currentDate}</h1>
+              <h1>{currentTime}</h1>
+            </div>
+            <div className='h-[70%] bg-[#101744]'></div>
+          </div>
+          {/* stopwatch */}
+          <div className="row-span-2 col-span-2 rounded-2xl">
+            <Stopwatch />
+          </div>
         </div>
+        {/* browse button for navigation */}
         <div className="h-[10vh] flex items-center justify-end">
-          <button className="bg-green-600 w-32 h-8" type="submit">
+          <button onClick={handleClick} className="bg-[#148A08] w-32 h-8 rounded-2xl text-white font-semibold tracking-wide cursor-pointer">
             Browse
           </button>
         </div>
