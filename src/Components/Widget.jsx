1cbe00f0e0287786a561e46997c070eccc9e0c27
sumit-616 <sumit616794@gmail.com>
2025-03-16 14:24:09 +0530

    Update Widget.jsx

--- src/Components/Widget.jsx
+++ src/Components/Widget.jsx
@@ -51,28 +51,28 @@ const Widget = () => {
 
   return (
     <div className="h-screen w-[100vw] bg-black overflow-y-auto" style={{ width: contentWidth }}>
-      <div className='mx-6 mt-8 mb-2'>
-        <div className="h-[120vh] grid md:grid-cols-3 md:grid-rows-5 sm:grid-cols-1 gap-x-6 gap-y-4">
+      <div className='mx-6 mt-2 mb-2'>
+        <div className="h-[100vh] grid md:grid-cols-3 md:grid-rows-5 sm:grid-cols-1 gap-x-12 gap-y-4">
           {/* userDp first one */}
           <div className="flex items-center bg-[#5746EA] row-span-2 py-6 rounded-2xl px-6 ">
             <img className='h-full w-28' src="/card_img/boydp.png" alt="" />
-            <div className='flex flex-col px-8 gap-4'>
+            <div className='flex flex-col px-6 gap-4'>
               <div className='text-white'>
                 <h3 className='text-xl'>{userData?.name}</h3>
                 <h3 className='text-xl'>{userData?.email}</h3>
                 <h1 className='text-3xl font-bold'>{userData?.userName}</h1>
               </div>
-              <div className='grid md:grid-cols-2 gap-2'>
+              <div className='grid md:grid-cols-2 gap-x-6 gap-y-2'>
                 {selectedGenres?.map((genre, index) => (
                   <div key={index}>
-                    <div className='text-white rounded-2xl text-md h-8 bg-[#9F94FF] flex justify-center items-center'>{genre}</div>
+                    <div className='text-white rounded-2xl text-md h-8 w-24 bg-[#9F94FF] flex justify-center items-center'>{genre}</div>
                   </div>
                 ))}
               </div>
             </div>
           </div>
           {/* notes second one */}
-          <div className="flex flex-col gap-4 bg-[#F1C75B] row-span-3 p-8 rounded-2xl">
+          <div className="flex flex-col gap-2 bg-[#F1C75B] row-span-3 p-4 rounded-2xl">
             <h1 style={{ font: "Roboto" }} className='text-black text-3xl font-bold'>All notes</h1>
             <textarea className='w-full h-full p-2 border-2 rounded-lg' name="notes" id="notes" placeholder='This is how I am going to learn MERN Stack in next 3 months.'></textarea>
           </div>
